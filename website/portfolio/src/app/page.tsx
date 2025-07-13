43d30e3e2d2718ab7e9c790c83d1a3ef3ebdaaa4
Thobla <thorgalblanco@gmail.com>
2025-07-13 16:09:01 +0200

    Feat: Happy with the NavBar

--- website/portfolio/src/app/page.tsx
+++ website/portfolio/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 
 
-
 export function Parimg() {
+    const mytest = "test";
     return (
         <div className = "flex w-1/1 align-center bg-blue-300">
-            <p className="flex-auto w-2/3 bg-orange-300 text-center text-4xl">Home</p>
+            <p className="flex-auto w-2/3 bg-orange-300 text-center text-4xl">{mytest}</p>
             <Image className="flex-auto w-1/3 rounded-full object-cover" // aspect-square
             width = {1}
             height = {1}
--- website/portfolio/src/app/ui/navlinks.tsx
+++ website/portfolio/src/app/ui/navlinks.tsx
@@ -6,16 +6,24 @@ import clsx from 'clsx'
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard'},//, icon: HomeIcon },
+  { name: 'Home', href: '/'},//, icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
+    name: 'Projects',
+    href: '/projects',
     //icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers'},//, icon: UserGroupIcon },
+  { name: 'Hobbies', href: '/hobbies'},//, icon: UserGroupIcon },
+  { name: 'Ask Nils', href: '/ask_nils'}
 ];
 
-export default function NavLinks() {
+const refToTitle : {[key: string] : string} = { '/' : 'Thorgal.no',
+'/projects' : 'Projects',
+'/hobbies' : 'Hobbies',
+'/ask_nils' : 'Ask Nils'}
+
+
+
+export function NavLinks() {
     const pathname = usePathname();
   return (
     <>
@@ -26,14 +34,29 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx(`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3`, 
-                            {'bg-sky-100 text-blue-600': pathname === link.href,},
+            className={clsx(`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-blue-600 p-3 text-sm font-medium hover:bg-gray-50 hover:text-black md:flex-none md:justify-start md:p-2 md:px-3`, 
+                            {'bg-sky-200 text-blue-800': pathname === link.href,},
             )}
           >
-            <p className="hidden md:block">{link.name}</p>
+            <p>{link.name}</p>
           </Link>
         );
       })}
     </>
   );
 }
+
+export function NavTitle() {
+    const pathname = usePathname();
+    const val = refToTitle[pathname];
+      return(
+        <div
+        className="mb-2 flex items-end justify-start rounded-md bg-blue-600 p-8"
+      >
+        <div className="w-32 text-white text-center md:w-40">
+            <h1>{val}</h1>
+        </div>
+      </div>)
+}
+
+
