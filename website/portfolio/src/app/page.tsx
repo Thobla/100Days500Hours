9182cf767cbd449cb436f1f65450d1b223b546d7
Thobla <thorgalblanco@gmail.com>
2025-07-31 13:39:04 +0200

    feat: Added simple homepage

--- website/portfolio/src/app/page.tsx
+++ website/portfolio/src/app/page.tsx
@@ -1,21 +1,35 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 
 function Parimg() {
-    const mytest = "test";
+    const mytest = "I am Thorgal Blanco";
     return (
-        <div className = "flex w-1/1 align-center bg-blue-300">
-            <p className="flex-auto w-2/3 bg-orange-300 text-center text-4xl">{mytest}</p>
-            <Image className="flex-auto w-1/3 rounded-full object-cover" // aspect-square
-            width = {1}
-            height = {1}
-            src = '/window.svg'
+        <div className = "flex w-1/1 align-center">
+            <div className="flex-auto w-3/5">
+                <p className=" ml-10 mt-20 text-4xl">I am Thorgal,
+                <span className="block ml-6 text-blue-500 text-3xl">Developer and Nerd</span>
+                <span className="block mt-10 text-2xl">Feel free to check out some of my personal projects in 
+                the <Link className="text-blue-500" href="/Projects">Projects</Link> directory.
+                </span>
+                </p>
+            </div>
+            <Image className="flex-auto w-2/5 rounded-full object-cover m-6 border-5 border-blue-500" // aspect-square
+            width = {100}
+            height = {100}
+            src = '/avatar.png'
             alt = 'Beautiful portrait of moi'/>
-        </div>)
+        </div>
+        )
 }
 
 export default function Home() {
     return (
+        <>
+        <div className="flex w-1/1 align-center bg-black min-h-3">
+            <p className="flex-auto text-white pb-5 pt-3 text-center text-4xl">Welcome to my website :)</p>
+        </div>
         <Parimg/>
+        </>
         )
 }
