3dd6291ebb5a80678c7a808e93a0462b1e67a71c
Thobla <thorgalblanco@gmail.com>
2025-08-01 15:04:46 +0200

    feat: made sindre and chess pages

--- website/portfolio/src/app/projects/ProjectPages/Chess.tsx
+++ website/portfolio/src/app/projects/ProjectPages/Chess.tsx
@@ -1,6 +1,34 @@
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Sindre(){
     return (
-        <h1>I am Chess</h1>
+        <div className = "flex flex-wrap flex-col items-center  w-full">
+            <div className = "flex flex-row object-contain flex-grow h-30">
+                <Image src="/chessLogo.png"
+                width={1000} height={1000}
+                alt="Jorden"
+                className="object-contain w-full h-full"
+                />
+            </div>
+            <div className="mt-10 ml-10 mr-10">
+                <Image 
+                    className="float-right w-2/5 h-2/5 ml-2"
+                    src="/chess.gif"
+                    width={100}
+                    height={100}
+                    alt="Sindre.gif"/>
+                <p className="pt-2">Ever since its beta release in the 6th century, chess has not received any major 
+                updated or patches. Atleast this was the case until april of 2022, when chess was re-released 
+                as a 2d-platformer under its new name "corrupted chess"
+                <span className="block pt-2">
+                    Corrupted chess was a group project developed in regards to the class INF112-Introduction to system 
+                    development at the university of Bergen. Our group consisted of six people, each with our own main 
+                    responsibility---my responsibility was code and quality. The development was collectively managed, and 
+                    overall a great experience to be a part of.
+                </span>
+                </p>
+            </div>
+        </div>
     )
 }
